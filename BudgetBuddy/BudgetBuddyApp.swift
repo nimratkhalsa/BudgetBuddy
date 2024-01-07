c1529914943cc48285507bd2fd9f9942de20cb4b
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2024-01-07 23:58:35 +0530

    Initial Commit

---
+++ BudgetBuddy/BudgetBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBuddyApp.swift
+//  BudgetBuddy
+//
+//  Created by Nimrat Khalsa on 07/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
